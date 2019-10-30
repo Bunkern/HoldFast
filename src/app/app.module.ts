5e8bca175b1d004feb411e4584c0a5624763d77b
kenneth kruse Sørensen <Bunkern@users.noreply.github.com>
2019-10-30 14:15:23 +0100

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MainComponent } from './main/main.component';
+import { FrontComponent } from './front/front.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    MainComponent,
+    FrontComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
