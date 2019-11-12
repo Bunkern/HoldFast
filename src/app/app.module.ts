f2240eee838881be4ced2f3af0614a61a247ee9f
kenneth kruse Sørensen <Bunkern@users.noreply.github.com>
2019-11-12 08:29:11 +0100

    Menu made responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MainComponent } from './main/main.component';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 
 
 @NgModule({
@@ -18,6 +20,12 @@ import { MainComponent } from './main/main.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
